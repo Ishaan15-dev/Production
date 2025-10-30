7cd5c52b3de639db81c650f18a925af27bd931ad
snehajoshi <snehajoshi060@gmail.com>
2025-10-30 13:09:41 +0530

    done

--- Wrapper-Code/Env/Prod/Network-Skeleton/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "prod-otms-bucket"
-    key            = "env/prod/module/network-skeleton/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "otms-tf-lock-table"
-  }
-}
